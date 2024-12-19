32319625e507f2e70d64578554108f56ccdcb0f4
vuchu1 <timakalimanov2@gmail.com>
2024-12-19 09:32:44 +0300

    Task1.c

--- 3824Б1ПР6/08 Калиманов Тимур/task1.c
+++ 3824Б1ПР6/08 Калиманов Тимур/task1.c
@@ -1,7 +1,21 @@
-// Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
-// не менее двух нулей. Найти сумму элементов, расположенных между последними 
-// двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1111111111;
+    int zerolast = -1;
+    int zerolast2 = -1; 
+    int sum = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        if (A[i] == 0) {
+           zerolast2 = zerolast; 
+           zerolast = i;
+        }
+    }
+
+    if (zerolast != -1 && zerolast2 != -1) {
+        for (size_t i = zerolast2 + 1; i < zerolast; i++) {
+            sum += A[i];
+        }
+    }
+
+    return sum;
 }
